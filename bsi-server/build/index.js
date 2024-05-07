60b1b30a4f63ed6bc920d8a4746eefdee4b63b33
arenazl <arenazl@gmail.com>
2024-05-07 01:19:08 -0300

    ok

--- bsi-server/build/index.js
+++ bsi-server/build/index.js
@@ -45,7 +45,7 @@ class Server {
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
-            console.log('Server on port', this.app.get('port'));
+            console.log('Server on port ', this.app.get('port'));
         });
     }
 }
