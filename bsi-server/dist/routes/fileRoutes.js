13bfe827bc24f877ca381561f70797ccc9d973e2
arenazl <arenazl@gmail.com>
2024-05-20 00:59:42 -0300

    ok

--- bsi-server/dist/routes/fileRoutes.js
+++ bsi-server/dist/routes/fileRoutes.js
@@ -11,6 +11,7 @@ class FileRoutes {
         this.config();
     }
     config() {
+
         this.router.get('/', filesController_1.default.list);
         //this.router.post('/', gamesController.create);
         //this.router.put('/:id', gamesController.update);
@@ -19,6 +20,7 @@ class FileRoutes {
         this.router.post('/uploadtr', filesController_1.default.uploadTR);
         this.router.get('/responsetr/:id', filesController_1.default.getResponseTR);
         this.router.get('/responsetrlist', filesController_1.default.getResponseTRList);
+    
         this.router.post('/dropbox', filesController_1.default.dropbox);
         this.router.get('/download/:id', filesController_1.default.downloadFile);
         //this.router.post('/file', gamesController.file);
