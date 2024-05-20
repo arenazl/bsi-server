70513d689f4016aaaea2912bc0b3676135235918
arenazl <arenazl@gmail.com>
2024-05-20 01:05:13 -0300

    ok

---
+++ bsi-server/build/keys.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        //host: 'us-cluster-east-01.k8s.cleardb.net',
+        //user: 'bbb88ce4bf618b', 
+        //password: '5883f2bf',
+        host: 'us-cluster-east-01.k8s.cleardb.net',
+        user: 'bbb88ce4bf618b',
+        password: '5883f2bf',
+        database: 'heroku_73c30f730ac0413',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    },
+    AWS: {
+        bucketName: 'sisbarrios',
+        bucketRegion: 'sa-east-1',
+        accesKey: 'AKIATI3QXLJ4VE3LBKFN',
+        secretKey: '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'
+    },
+    mails: {
+        documentacion: 'sisbarrios',
+        control: 'Proyectos.don.luisk41@gmail.com',
+        new: 'Marcela@newlife.com',
+        cj: 'bienescintiap@gmail.com ',
+        real: 'pablorealstate@hotmail.com',
+        ar: 'desarrolloinmobiliarioar@hotmail.com',
+        admin: 'arenazl@gmail.com'
+    }
+};
+/*
+b2b686e8c89af3
+b7745d1b
+us-cdbr-east-05.cleardb.net
+heroku_55504b2b2691e53
+*/ 
